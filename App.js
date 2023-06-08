1b60120a4a54415258d708fb59ef7004a8c5808f
Precell <precell@uncommon.org>
2023-06-08 10:48:41 +0200

    added flex box

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyb
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
+import Sandbox from './components/sandbox';
 
 
 export default function App() {
@@ -39,6 +40,7 @@ export default function App() {
   }
   
   return (
+    // <Sandbox/>
     <TouchableWithoutFeedback onPress={() =>{console.log('dismissed KB'); Keyboard.dismiss()}}>
       <View style={styles.container}>
         <Header />
@@ -66,9 +68,11 @@ const styles = StyleSheet.create({
 
   },
   content:{
-    padding:40
+    padding:40,
+    flex:1
   },
   list:{
-    marginTop:20
+    marginTop:20,
+    flex:1
   }
 });
